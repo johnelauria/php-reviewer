b149c554597c44d2e70a257463ca6ce5ae5f55e6
John Maenard Elauria <johnelauria@hotmail.com>
2017-12-11 23:36:06 +0800

    Add "About app"

--- app/src/main/java/com/johngeli/zendreviewer/MainActivity.kt
+++ app/src/main/java/com/johngeli/zendreviewer/MainActivity.kt
@@ -44,11 +44,11 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_about -> {
+                startActivity(Intent("android.intent.action.ABOUT_APP"))
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
