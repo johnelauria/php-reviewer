196a2d8b66094d4adc5aa2fb6334b189c6c52a9f
John Maenard Elauria <johnelauria@hotmail.com>
2017-12-16 13:00:05 +0800

    Delete zce logos

--- app/src/main/java/com/twopixeled/zendreviewer/AboutAppActivity.kt
+++ app/src/main/java/com/twopixeled/zendreviewer/AboutAppActivity.kt
@@ -2,11 +2,17 @@ package com.twopixeled.zendreviewer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import kotlinx.android.synthetic.main.activity_about_app.dev1TV
+import kotlinx.android.synthetic.main.activity_about_app.dev2TV
 
 class AboutAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_app)
+
+        dev1TV.movementMethod = LinkMovementMethod.getInstance()
+        dev2TV.movementMethod = LinkMovementMethod.getInstance()
     }
 }
